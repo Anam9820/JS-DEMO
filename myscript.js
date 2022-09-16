11592b96df5123c70ad52618dff3815a7ff12615
Anam9820 <khan.anam.19bsc203@gmail.com>
2022-09-16 21:18:22 +0530

    upload new file

---
+++ myscript.js
@@ -0,0 +1,22 @@
+console.log("hellothere123")
+
+let message="good morning";
+console.log(message)
+alert(message);
+
+var greeting=("good to see")
+console.log(greeting)
+alert(greeting);
+
+let messagebox;
+messagebox = "nice";
+messagebox = "to see";
+console.log(messagebox);
+
+let admin;
+let name;
+name ="anam";
+admin = name;
+alert(admin);
+
+
